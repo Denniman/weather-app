e5a2f4585fb7e54cf09dc8614a632c794947dd1f
Dennis <dennimanadams@gmail.com>
2020-09-03 23:23:50 +0100

    prevent default submit behavior

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const searchInput = document.querySelector('#city-search');
+const form = document.querySelector('form');
+
+
 const key = 'b21f31efe5b1f8344d5a80a82c7bd1d6'
 
 const URL = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
@@ -8,7 +12,15 @@ async function getWeatherByLocation(location) {
     });
     const resData = await res.json();
 
-    // console.log(resData);
+    // console.log(resData, kelvinToCelsius(resData.main.temp));
 }
 
 getWeatherByLocation('Yenagoa');
+
+function kelvinToCelsius(k) {
+    return (k - 273.15).toFixed(2);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
