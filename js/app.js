03dbe860ba3bbc1328c8cdc73e4d3f825c62fc71
Dennis <dennimanadams@gmail.com>
2020-09-03 23:00:03 +0100

    add weather api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("connected!");
+const key = 'b21f31efe5b1f8344d5a80a82c7bd1d6'
+
+const URL = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+
+async function getWeatherByLocation(location) {
+    const res = await fetch(URL(location), {
+        origin: "cors"
+    });
+    const resData = await res.json();
+
+    // console.log(resData);
+}
+
+getWeatherByLocation('Yenagoa');
