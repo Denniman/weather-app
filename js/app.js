7d6365022fd49d8c834ba508707259c8aa4b91ea
Dennis <dennimanadams@gmail.com>
2020-09-04 12:38:27 +0100

    add api data to dom

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const form = document.querySelector('form');
 const displayResult = document.querySelector('.weather-result');
 
 
-const key = 'b21f31efe5b1f8344d5a80a82c7bd1d6'
+const key = 'a11f31efe5b1f8344d5a80a82c7bd1d6'
 
 const URL = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 
@@ -13,13 +13,20 @@ async function getWeatherByLocation(city) {
     });
     const resData = await res.json();
 
-    // console.log(resData, kelvinTocityresData.main.temp));
+    addWeatherToPage(resData);
+}
+
+function addWeatherToPage(data) {
+    const temp = kelvinToCelsius(data.main.temp);
+
+    const weather = document.querySelector('.header-city-sub-degree');
+    weather.textContent = `${temp}°C`
 }
 
 getWeatherByLocation('Yenagoa');
 
 function kelvinToCelsius(k) {
-    return (k - 273.15).toFixed(2);
+    return (k - 273.15).toFixed();
 }
 
 form.addEventListener('submit', (e) => {
